c0693fce86c94133220c40a3f5cb3000d956a39e
Amarilis Pequeno <amarilispequeno@gmail.com>
2023-03-15 21:09:48 -0300

    Exercicio08

---
+++ app/js/controllers/negociacaoControllers.js
@@ -0,0 +1,50 @@
+import { Negociacao } from "../models/negociacao.js";
+import { ListaNegociacao } from "../models/listaNegociacao.js";
+import { NegociacoesView } from "../views/negociacoesViews.js";
+
+export class NegociacaoController {
+  #campoData;
+  #campoQuantidade;
+  #campoValor;
+
+  #listaNegociacao = new ListaNegociacao();
+  #negociacoesView = new NegociacoesView();
+
+  // construtor que pega os elementos do formulario
+  constructor() {
+    //pega o campo de data do formulario e salva e atribui ao #campoData
+    this.#campoData = document.getElementById("data");
+    this.#campoQuantidade = document.getElementById("quantidade");
+    this.#campoValor = document.getElementById("valor");
+    this.filtro = document.getElementById("valorFiltro");
+  }
+
+  criarNegociacao() {
+    //instacia um objeto do tipo data
+    let data = new Date(
+      this.#campoData.value + "T00:00:00-03:00"
+    ).toLocaleDateString();
+    // cria uma variavel com nome quantidade e atribui a variavel do construtor pegando os valores
+    let quantidade = parseInt(this.#campoQuantidade.value);
+    let valor = parseFloat(this.#campoValor.value);
+
+    /*cria uma variavel comnome negociaçao que recebe uma nova negociaçao da classe Negociaçao dando como parametro os objetos acima data, quantidade e valor*/
+    let negociacao = new Negociacao(data, quantidade, valor);
+    this.#listaNegociacao.adicionarNegociacao(
+      data,
+      quantidade,
+      valor,
+      negociacao
+    );
+    this.#negociacoesView.atualizarTabela(data, quantidade, valor);
+  }
+
+  filtrarNegociacao() {
+    let valorFiltro = this.filtro.value;
+    console.log(valorFiltro);
+    let negociacoesFiltradas =
+      this.#listaNegociacao.filtrarNegociacoesPorValor(valorFiltro);
+    console.log(negociacoesFiltradas);
+    this.#negociacoesView.atualizarFiltro(negociacoesFiltradas);
+  }
+}
---
+++ app/js/views/negociacoesViews.js
@@ -0,0 +1,18 @@
+export class NegociacoesView {
+  //esse aqui um metodo que vai atualizar a tabela no html.
+  atualizarTabela(data, quantidade, valor) {
+    //aqui estamos usando o dom para pegar a tag tbody atraves do id que também chama tbody. a let vai armazenar.
+    let corpoTabela = document.getElementById("tbody");
+
+    if (corpoTabela) {
+      let tr_row = corpoTabela.insertRow(0);
+      let td_data = tr_row.insertCell(0);
+      let td_quantidade = tr_row.insertCell(1);
+      let td_valor = tr_row.insertCell(2);
+
+      td_data.innerHTML = data;
+      td_quantidade.innerHTML = quantidade;
+      td_valor.innerHTML = valor;
+    }
+  }
+}
